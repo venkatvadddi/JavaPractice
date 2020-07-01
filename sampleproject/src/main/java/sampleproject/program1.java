47f5b28286d5a03c7db4da25327bdbed160a65d5
venkatvadddi <67677829+venkatvadddi@users.noreply.github.com>
2020-07-01 13:08:57 +0530

    Arrarysortingprog
    programe to sort the list of strings using Java collection?

---
+++ sampleproject/src/main/java/sampleproject/program1.java
@@ -0,0 +1,31 @@
+package sampleproject;
+import java.util.*; 
+
+
+//Write code to sort the list of strings using Java collection
+public class program1 {
+	
+		public static void main(String[] args) 
+		{ 
+			String input[] = { "January", "February", "March", "April", "May", "June", "July",
+									"august", "September", "October", "november", "December" } ;
+			
+			List<String> list = new ArrayList<String>(); 
+			  
+	        // Iterate through the array 
+	        for (String val : input) { 
+	            // Add each element into the list 
+	            list.add(val); 	            
+	        } 	 
+	        System.out.println("Converted list:\n" + list);
+	        
+			// using Collections.sort method we can arrage elements in ascending order. */
+			Collections.sort(list); 
+			System.out.println("Sorting order for given values:\n" + list); 
+			
+			Collections.sort(list, String.CASE_INSENSITIVE_ORDER);
+			System.out.println("Sorting order with case sensitive:\n" + list); 
+		} 
+		
+
+}
